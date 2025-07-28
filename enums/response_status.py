4a64582e1a617f2835fc9319c482cec34022c71d
serj2626 <serj2626@mail.ru>
2025-07-29 01:17:10 +0300

    refactor: formatting code

--- enums/response_status.py
+++ enums/response_status.py
@@ -1,8 +1,9 @@
 from enum import Enum
 
+
 class ResponseStatus(str, Enum):
-    sent = "sent"                   # Отклик отправлен
-    viewed = "viewed"               # Работодатель посмотрел
-    rejected = "rejected"           # Отказ
-    approved = "approved"           # Принят, продолжение общения
-    interview = "interview"         # Приглашение на интервью
+    sent = "sent"  # Отклик отправлен
+    viewed = "viewed"  # Работодатель посмотрел
+    rejected = "rejected"  # Отказ
+    approved = "approved"  # Принят, продолжение общения
+    interview = "interview"  # Приглашение на интервью
